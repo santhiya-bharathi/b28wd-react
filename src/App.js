83a80ebce6bb4fe8f879a8331985d462fb59f94d
santhiya-bharathi <santhiyabharathi1999@gmail.com>
2021-11-16 19:16:58 +0530

    moviedetailsapp

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ import EditIcon from '@mui/icons-material/Edit';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 
 import { useHistory } from "react-router-dom";
@@ -25,57 +29,70 @@ export default function App() {
   const INITIAL_MOVIES = [{
     name:"Avatar",
     pic:"https://cdna.artstation.com/p/assets/images/images/031/645/214/large/shreyas-raut-avatar-2.jpg?1604210989&dl=1",
-    rating:"7.8",
+    rating:7.8,
     summary:"On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids. Jake Sully a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Na'vi woman. As a bond with her grows, he is drawn into a battle for the survival of her world.",
     trailer:"https://www.youtube.com/embed/5PSNL1qE6VY"  
   },
 {
   name:"Master",
   pic:"https://pbs.twimg.com/media/ENHK08SUEAEdnAx.jpg",
-  rating:"7.8",
+  rating:7.8,
   summary:"Troubled alcoholic teacher JD is sent to teach at a juvenile reform school. But when he realises a dangerous criminal is using his students to cover up his crimes, JD sets out to stop him. Strong violence, drug misuse.",
   trailer:"https://www.youtube.com/embed/UTiXQcrLlv4"
 },
 {
   name:"96",
   pic:"https://moviegalleri.net/wp-content/uploads/2018/07/Trisha-Krishnan-Vijay-Sethupathi-96-Movie-New-Poster.jpg",
-  rating:"8.6",
+  rating:8.6,
  summary:"K Ramachandran, a photographer, gets nostalgic after he visits his school in his hometown. During a reunion with his classmates, he meets Janaki, his childhood sweetheart.",
  trailer:"https://www.youtube.com/embed/r0synl-lI4I"
 },
 {
   name:"Pelli Choopulu",
   pic:"https://wallpapercave.com/wp/wp7024418.jpg",
-  rating:"8.2",
+  rating:8.2,
  summary:"Vijay's father, who is fed up with Vijay's carefree life, decides to make him marry a girl in hopes that he might become a responsible man. However, meeting an ambitious girl changes his life for the better.",
  trailer:"https://www.youtube.com/embed/9v9nESxBpqU"
 },
  {
   name:"Jagame Thandhiram",
   pic:"https://www.kollywoodzone.com/data/media/11148/jagame_thanthiram_movie_posters_03.jpg",
-  rating:"6",
+  rating:6,
  summary:"Suruli, a kind-hearted gangster, goes to London and gets embroiled in several criminal activities. However, the stakes get higher when he falls in love with a beautiful singer named Attila.",
  trailer:"https://www.youtube.com/embed/2OtgYcd83Qg"
 },
  {
   name:"squid game",
   pic:"https://cdn.vox-cdn.com/thumbor/vmRga6mG5sihYrHyZ43WswwtyIU=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22911132/EN_SQdGame_Main_PlayGround_Horizontal_RGB_PRE.jpeg",
-  rating:"8.1",
+  rating:8.1,
  summary:"Squid Game's basic story revolves around 456 heavily debt-ridden people from different age groups and strata of society, who participate in six rounds of various children's games to win a humongous sum of money.",
  trailer:"https://www.youtube.com/embed/oqxAJKy0ii4"
 }
 ];
 const [movies, setMovies] = useState(INITIAL_MOVIES);
 const history = useHistory();
-
+const [mode, setMode] = useState("dark");
+const darkTheme = createTheme({
+  palette: {
+    mode: mode,
+  },
+});
   return (
+
+    // themeprovider for dark and light mode
+      // paper for blackground
+    <ThemeProvider theme={darkTheme}>
+      <Paper elevation={3} style={{borderRadius:"0px",minHeight:"100vh"}}>
+   
     <div className="App">
        <AppBar position="static">
        <Toolbar>
        <Button varient="text" color="inherit" onClick={()=>history.push("/")}>Home</Button>
        <Button varient="text" color="inherit" onClick={()=>history.push("/addmovies")}>AddMovies</Button>
        <Button varient="text" color="inherit" onClick={()=>history.push("/movielist")}>Movielist</Button>
-       {/* <Button varient="text" color="inherit" onClick={()=>history.push("/addcolor")}>Addcolor</Button> */}
+
+       
+       <Button varient="text" color="inherit" style={{marginLeft:"auto"}} onClick={()=>setMode(mode==="light"? "dark":"light")}> {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />} {mode==="light"? "Dark":"Light"}Mode</Button>
        </Toolbar>
        </AppBar>
 
@@ -105,24 +122,20 @@ const history = useHistory();
         <MovieList movies={movies} setMovies={setMovies}/>
         </Route>
 
-        {/* <Route path="/addcolor">
-        <AddColor/>
-        </Route> */}
-
-
         <Route path="**">
           <NotFound/>
         </Route>
 
-        
-
       </Switch>
    
     </div>
+    </Paper>
+    </ThemeProvider>
   );
 }
 //when two  components needs the same data(movies)-> put the data in the common parent component (App) - HOC - Higher order components 
 
+//creating not found page
 function NotFound(){
   return(
     <div className="not-found-pic">
@@ -131,6 +144,7 @@ function NotFound(){
     </div>
   );
 }
+//creating home page
 function Home() {
   return (
     <div className="home">
@@ -140,6 +154,7 @@ function Home() {
   );
 }
 
+// moviedetails code for when we click the info button 
 function MovieDetails({movies}){
   const history = useHistory();
   const {id} = useParams();
@@ -165,6 +180,7 @@ function MovieDetails({movies}){
   );
 }
 
+// editmovie code for edit the information
 function EditMovie({movies,setMovies}){
   const history = useHistory();
   const {id} = useParams();
@@ -209,6 +225,8 @@ function EditMovie({movies,setMovies}){
     </div>
   );
 }
+
+//for adding the movie details to movielist
 function AddMovie({movies,setMovies}){
   const history = useHistory();
   const [name, setName] = useState("");
@@ -248,6 +266,7 @@ const addMovie =()=>{
   );
 } 
 
+// this code for like and dislike function
 function Counter(){
   const [like, setLike] = useState(0);
   const [disLike, setDisLike] = useState(0);
@@ -263,6 +282,8 @@ function Counter(){
   </div>
   );
 }
+
+// this is the movie details delete and edit code
 function MovieList({movies, setMovies}){
   const history = useHistory();
   return(
@@ -291,6 +312,7 @@ function MovieList({movies, setMovies}){
   );
 }
 
+// movie details code
 function Movie({pic, name, rating, summary,id,deleteButton,editButton}){
 const [show,setShow] = useState(true);
 const history = useHistory();
@@ -339,31 +361,3 @@ const summaryStyles = {
 }
 
 
-// function AddColor(){
-//   const [color, setColor] = useState("pink");
-// const styles = {backgroundColor: color};
-// const [colors, setColors] = useState(["teal", "orange"]);
-//   return(
-//     <div>
-//       <input
-//       value={color}
-//       onChange={(event)=>setColor(event.target.value)}
-//       style={styles}
-//       placeholder="enter a color"
-//       />
-       
-//       <button onClick={()=> setColors([...colors, color])}>Add color</button>
-//       {colors.map((clr,index)=>(<ColorBox key={index} color={clr}/>))}
-//     </div>
-//   );
-// }
-
-// function ColorBox({color}){
-//   const styles = {
-//     backgroundColor:color,
-//     height: "40px",
-//     width:"600px",
-//     marginTop:"10px",
-//   };
-//   return <div style = {styles}></div>
-// }
